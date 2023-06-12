f2115141493b19b755650e3898a8858836db0bcf
samandcode <chijiokeemechebe@gmail.com>
2023-06-12 23:35:05 +0100

    Login and Pass Reset

---
+++ app/auth/resetpassword/page.tsx
@@ -0,0 +1,104 @@
+'use client'
+import { useState } from 'react';
+import Link from 'next/link';
+import { Eye, EyeSlash } from 'iconsax-react';
+
+function ResetPassword() {
+    const [password, setPassword] = useState('');
+    const [newPassword, setNewPassword] = useState('');
+
+    const [passwordShown, setPasswordShown] = useState(false);
+    const togglePasswordVisiblity = () => {
+        setPasswordShown(passwordShown ? false : true);
+    };
+
+    return (
+        <div className='bg-white w-full h-screen'>
+            <div className='flex items-center justify-center w-full h-full'>
+                <div className='grid grid-cols-1 md:grid-cols-4 gap-3 w-full h-screen items-center mx-auto'>
+
+                    <div className='md:flex hidden col-span-3 bg-yellow-400 h-screen'>
+                        <div className='w-full h-full bg-[url(https://images.unsplash.com/photo-1566073771259-6a8506099945?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80)] bg-center bg-no-repeat bg-cover'>
+                            <div className='w-full h-full flex bg-gradient-to-t from-black/90 to-black/20'>
+                                <div className='text-xs font-normal leading-6 text-white fixed bottom-0 left-0 w-full p-8'>
+                                    <p>Copyright Mybcloud © {new Date().getFullYear()}</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className='col-span-full md:col-span-1 bg-white h-auto m-auto md:p-0 p-4'>
+                        <div className="flex justify-center items-center p-5 md:p-7 w-full h-full bg-white md:drop-shadow-none drop-shadow-md m-auto">
+                            <div className="m-0">
+                                <p
+                                    tabIndex={0}
+                                    role="heading"
+                                    aria-label="Login to your account"
+                                    className="text-xl font-semibold leading-8 text-gray-900 text-left"
+                                >
+                                    Reset Password 🔒
+                                </p>
+                                <span className='text-gray-600 text-sm font-normal leading-5'>
+                                    Your new password must be different from previously used passwords
+                                </span>
+                                <form className='flex flex-col space-y-5 w-full mt-6'>
+                                    <div className="relative flex items-center justify-center">
+                                        <input
+                                            type={passwordShown ? 'text' : 'password'}
+                                            role="input"
+                                            placeholder="New Password"
+                                            className="bg-white border-[1.3px] border-gray-300 rounded-md focus:outline-none text-sm font-normal leading-none text-gray-900 py-3 w-full pl-3 placeholder:font-normal placeholder:text-sm"
+                                            required
+                                            value={password}
+                                            onChange={(e) => setPassword(e.target.value)}
+                                        />
+                                        <div
+                                            className="absolute inset-y-0 right-0 text-gray-500 hover:text-gray-800 flex items-center pr-4 cursor-pointer"
+                                            onClick={togglePasswordVisiblity}
+                                        >
+                                            {passwordShown ? <EyeSlash size={18} /> : <Eye size={18} />}
+                                        </div>
+                                    </div>
+
+                                    <div className="relative flex items-center justify-center">
+                                        <input
+                                            type={passwordShown ? 'text' : 'password'}
+                                            role="input"
+                                            placeholder="Confirm New Password"
+                                            className="bg-white border-[1.3px] border-gray-300 rounded-md focus:outline-none text-sm font-normal leading-none text-gray-900 py-3 w-full pl-3 placeholder:font-normal placeholder:text-sm"
+                                            required
+                                            value={newPassword}
+                                            onChange={(e) => setNewPassword(e.target.value)}
+                                        />
+                                    </div>
+
+                                    <div className="mt-6">
+                                        <button
+                                            role="button"
+                                            type="submit"
+                                            aria-label="log into my account"
+                                            className="text-sm font-normal rounded leading-none text-white focus:outline-none bg-[#404040] disabled:bg-[#404040]/50 hover:disabled:text-white hover:text-gray-900 border hover:bg-yellow-500 transition-all py-3 w-full flex items-center justify-center gap-1 tracking-wide"
+                                            disabled={!password}
+                                        >
+                                            <span>Change Password</span>
+                                        </button>
+                                        <div className='flex justify-center mt-2'>
+                                            <span className='text-xs text-normal text-gray-500 leading-5 hover:text-gray-800 cursor-pointer'>
+                                                <Link href="/auth/login">
+                                                    <span className='hover:text-gray-800 hover:transition-colors'>Back to Login</span>
+                                                </Link>
+                                            </span>
+                                        </div>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default ResetPassword
--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
-import Image from 'next/image'
+import Image from 'next/image';
+import Link from 'next/link';
 
 export default function Home() {
   return (
@@ -9,7 +10,7 @@ export default function Home() {
           <code className="font-mono font-bold">app/page.tsx</code>
         </p>
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
+          <Link
             className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
             href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
             target="_blank"
@@ -24,7 +25,7 @@ export default function Home() {
               height={24}
               priority
             />
-          </a>
+          </Link>
         </div>
       </div>
 
@@ -40,7 +41,7 @@ export default function Home() {
       </div>
 
       <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
+        <Link
           href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
           target="_blank"
@@ -55,9 +56,9 @@ export default function Home() {
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
             Find in-depth information about Next.js features and API.
           </p>
-        </a>
+        </Link>
 
-        <a
+        <Link
           href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
           target="_blank"
@@ -72,9 +73,9 @@ export default function Home() {
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
             Learn about Next.js in an interactive course with&nbsp;quizzes!
           </p>
-        </a>
+        </Link>
 
-        <a
+        <Link
           href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
           target="_blank"
@@ -89,9 +90,9 @@ export default function Home() {
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
             Explore the Next.js 13 playground.
           </p>
-        </a>
+        </Link>
 
-        <a
+        <Link
           href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
           target="_blank"
@@ -106,7 +107,7 @@ export default function Home() {
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
             Instantly deploy your Next.js site to a shareable URL with Vercel.
           </p>
-        </a>
+        </Link>
       </div>
     </main>
   )
