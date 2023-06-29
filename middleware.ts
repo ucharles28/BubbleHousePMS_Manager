37cc978da80dcf1597907e50160c4d5a60c28f91
Ikechukwu Uzoma <uzomacharles28@outlook.com>
2023-06-29 22:07:24 +0100

    Update middleware

--- middleware.ts
+++ middleware.ts
@@ -3,7 +3,8 @@ import type { NextRequest } from 'next/server'
  
 // This function can be marked `async` if using `await` inside
 export function middleware(request: NextRequest) {
-  if (!request.nextUrl.pathname.startsWith('/login')) {
+  if (!request.nextUrl.pathname.startsWith('/login') || 
+  !request.nextUrl.pathname.startsWith('/resetpassword') || !request.nextUrl.pathname.startsWith('/forgotpassword')) {
     const cookie = request.cookies.get('logged-in')
     if (cookie) {
       if (cookie.value === "true") return;
