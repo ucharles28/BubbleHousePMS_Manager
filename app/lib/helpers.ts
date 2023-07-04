d803a071626fbde0d8646840b2afbfe2dd71f9c6
Ikechukwu Uzoma <uzomacharles28@outlook.com>
2023-07-04 08:48:53 +0100

    Update user info

--- app/lib/helpers.ts
+++ app/lib/helpers.ts
@@ -1,10 +1,12 @@
 import { User } from "../models/user"
 
 export async function getUserInfo() {
-    if (!process.env.HOST){
-        return { hotelId: '', userId: ''}
-    }
-    const result = await fetch(`${process.env.HOST}/api/users/info`)
-    // const result = await fetch(`http://localhost:3000/api/users/info`)
+    // const cookieStore = cookies()
+    // console.log(cookieStore.get('user')?.value)
+    // if (!process.env.HOST){
+    //     return { hotelId: '', userId: ''}
+    // }
+    // const result = await fetch(`${process.env.HOST}/api/users/info`)
+    const result = await fetch(`https://manager.mybcloud.com/api/users/info`)
     return await result.json() as User
 }
